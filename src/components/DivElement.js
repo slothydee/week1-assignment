345f4b38b0ffdc5807e345f60dbaa5cd742e24fa
SlothyDee <slothydeelight@gmail.com>
2021-04-14 18:24:14 -0700

    completed hw

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,9 @@
-// Import HTMLElement here
+import HTMLElement from './HTMLElement';
 
-// Define class here
-
-// Export class here
+export default class DivElement extends HTMLElement {
+  constructor(content) {
+    super(content);
+    this.content = content;
+    this.tag = 'div';
+  }
+}
